CyanogenMod/android_hardware_samsung_slsi-cm_exynos5433

mobicore: Silence date/time warnings
Change-Id: I9697c8bd27d190b5200d0e44067be325602ff0f1

mobicore/daemon/Daemon/Android.mk
@@ -8,7 +8,10 @@
 LOCAL_SRC_FILES += Daemon/MobiCoreDriverDaemon.cpp
 
 # Includes required for the Daemon
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/Daemon/public \
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/Daemon/public
+
+# Ignore non-reproducible date/time expansions
+LOCAL_C_FLAGS += -Wno-date-time
 
 # Internal components
 include $(LOCAL_PATH)/Daemon/Device/Android.mk

SerenityS/android_hardware_samsung_slsi-cm_exynos5430

mobicore/daemon/Daemon/Android.mk
@@ -8,7 +8,10 @@
 LOCAL_SRC_FILES += Daemon/MobiCoreDriverDaemon.cpp
 
 # Includes required for the Daemon
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/Daemon/public \
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/Daemon/public
+
+# Ignore non-reproducible date/time expansions
+LOCAL_CFLAGS += -Wno-date-time
 
 # Internal components
 include $(LOCAL_PATH)/Daemon/Device/Android.mk